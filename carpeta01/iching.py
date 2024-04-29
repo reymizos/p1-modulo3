12ea01bb05387005a6e23868a7bd9861670b6c3b
reymizos <162900838+reymizos@users.noreply.github.com>
2024-04-29 20:27:48 +0200

    Actualizando archivo iching.py como parte de 1.8
    Agregamos trigram2

--- carpeta01/iching.py
+++ carpeta01/iching.py
@@ -2,3 +2,6 @@ from random import choice, randint
 
 trigrama = ["Quian", "Kun", "Zhen", "Kan", "Gen", "Xun", "Li", "Dui"]
 trigram1 = choice(trigrama)
+# Se añade trigram2 -- Como parte de la respuesta de 1.8 --
+trigram2 = choice(trigrama)
+
